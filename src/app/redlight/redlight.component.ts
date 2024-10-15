6c69a8607f1ca82e437b20ff193be75b8ef53519
Puneet Sharma <puneetsharma9@hotmail.com>
2024-10-15 17:00:09 +0530

    update repo

--- src/app/redlight/redlight.component.ts
+++ src/app/redlight/redlight.component.ts
@@ -1,12 +1,28 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-redlight',
-  standalone: true,
-  imports: [],
-  templateUrl: './redlight.component.html',
-  styleUrl: './redlight.component.css'
+    selector: 'app-redlight',
+    standalone: true,
+    imports: [NgClass],
+    templateUrl: './redlight.component.html',
+    styleUrl: './redlight.component.css'
 })
 export class RedlightComponent {
+    constructor() {
+        this.loop();
+    }
+    active = 0;
+    loop() {
+        setInterval(() => {
+            console.log(this.active);
+            if (this.active == 10) {
+                this.active = 0;
+            } else {
+                this.active++;
+            }
+        }, 1000)
+    }
+
 
 }
